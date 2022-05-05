d5f10c886a913ebea0004cb232c13be21e7893b4
Ranidhumma <ranidhumma8@gmail.com>
2022-05-05 22:57:52 +0530

    creating projects for spring

---
+++ src/main/java/com/example/springcoreconcepts/controller/DemoBean.java
@@ -0,0 +1,9 @@
+package com.example.springcoreconcepts.controller;
+
+import org.springframework.stereotype.Component;
+    @Component
+    public class DemoBean {
+        public DemoBean(){}
+    }
+
+
